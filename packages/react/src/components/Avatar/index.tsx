1ec969a1a00725d1c68222f873a02cffeac4a908
renoeno <reno.less@gmail.com>
2022-10-25 15:52:57 -0300

    Add all components

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,19 @@
+import { User } from "phosphor-react";
+import { ComponentProps } from "react";
+import "./styles";
+import { AvatarContainer, AvatarFallback, AvatarImage } from "./styles";
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {
+
+}
+
+export function Avatar(props: AvatarProps){
+
+    return (
+    <AvatarContainer>
+        <AvatarImage {...props} />
+        <AvatarFallback delayMs={600}>
+            <User />
+        </AvatarFallback> 
+    </AvatarContainer>)
+}
